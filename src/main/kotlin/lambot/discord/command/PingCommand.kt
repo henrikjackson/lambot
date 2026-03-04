f452af558f4f8607d4ed2c4c2aaccc79e3414526
Henrik Jackson <henrik.mellum.jackson@mattilsynet.no>
2026-03-04 16:57:32 +0100

    Restrict /ping + make response ephemeral

--- src/main/kotlin/lambot/discord/command/PingCommand.kt
+++ src/main/kotlin/lambot/discord/command/PingCommand.kt
@@ -1,18 +1,21 @@
 package lambot.discord.command
 
-import dev.kord.core.behavior.interaction.respondPublic
+import dev.kord.core.Kord
+import dev.kord.core.behavior.interaction.respondEphemeral
 import dev.kord.core.event.interaction.GuildChatInputCommandInteractionCreateEvent
+import lambot.config.DiscordProperties
 import org.springframework.stereotype.Component
 
 @Component
-class PingCommand : SlashCommand {
+class PingCommand(private val discordProperties: DiscordProperties) : SlashCommand {
     override val name = "ping"
     override val description = "Check if the bot is alive"
+    override val requiredRoleIds: Set<String> get() = discordProperties.allowedRoleIds.toSet()
 
-    override suspend fun register(kord: dev.kord.core.Kord) {}
+    override suspend fun register(kord: Kord) {}
 
     override suspend fun handle(event: GuildChatInputCommandInteractionCreateEvent) {
-        event.interaction.respondPublic {
+        event.interaction.respondEphemeral {
             content = "Pong!"
         }
     }
